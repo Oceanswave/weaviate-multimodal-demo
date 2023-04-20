0f1f2f57815895e37950e3aacc611ed691da727e
Sean McLellan <Oceanswave@users.noreply.github.com>
2023-04-20 12:13:15 -0400

    Use weviate default url as a default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Query from "./Query/Query";
 const queryClient = new QueryClient();
 
 const Home: NextPage = () => {
-  const weaviateUrl = process.env.NEXT_PUBLIC_WEAVIATE_GRAPHQL_URL || "";
+  const weaviateUrl = process.env.NEXT_PUBLIC_WEAVIATE_GRAPHQL_URL || "http://localhost:8080/v1/graphql";
   return (
     <QueryClientProvider client={queryClient}>
       <Query weaviateUrl={weaviateUrl} />
